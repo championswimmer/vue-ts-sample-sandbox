17117a4f359c149daea339dc0cdcdd0f2a21c64e
Arnav Gupta <dev@championswimmer.in>
2018-10-01 00:37:20 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <Counter/>
+    <TSCounter/>
+  </div>
+</template>
+
+<script>
+import Counter from '@/components/Counter.vue'
+import TSCounter from '@/components/TSCounter.vue'
+
+export default {
+  components: { Counter, TSCounter },
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>JS Counter</h3>
+    <div>{{count}}</div>
+    <div>
+    <button @click="increment()">INCREMENT</button>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+export default  {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
+</script>
